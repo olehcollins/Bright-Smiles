b92b218358117dd61450ebaead350ffcf7f922d5
Oleh Collins <oleh.collins@icloud.com>
2024-09-07 22:01:05 +0100

    nothing

--- DentistAppointmentSystem.Tests/obj/Debug/net8.0/DentistAppointmentSystem.Tests.AssemblyInfo.cs
+++ DentistAppointmentSystem.Tests/obj/Debug/net8.0/DentistAppointmentSystem.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DentistAppointmentSystem.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d064e60005d72fd936ab7f84dcf90f32eb87fd1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47f6c34abc50a424363f8cd3479f4d79806004a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("DentistAppointmentSystem.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DentistAppointmentSystem.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
