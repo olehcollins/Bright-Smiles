b28c66852b7b6d25526cabcc1ed20e8ddd92d87f
Oleh Collins <oleh.collins@icloud.com>
2024-09-11 19:58:45 +0100

    user is able to upload profile photos, admin can edit or delete current staff

--- DentistAppointmentSystem/obj/Debug/net8.0/DentistAppointmentSystem.AssemblyInfo.cs
+++ DentistAppointmentSystem/obj/Debug/net8.0/DentistAppointmentSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DentistAppointmentSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39fe635d5c822ce40213516ea67c6728d494bf23")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eff4f7bd5021dd330fdf7e7318d16dd1f38d6dd0")]
 [assembly: System.Reflection.AssemblyProductAttribute("DentistAppointmentSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DentistAppointmentSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
